3f8289650eca19dce406feea85ead7fb7548de64
nesosuke <neso@neso.tech>
2022-04-22 16:24:23 +0900

    feat(frontend): :construction: wip&test: ファイルアップロードのテスト．

--- frontend/web.py
+++ frontend/web.py
@@ -1,8 +1,6 @@
-from crypt import methods
 # from flask_login import login_required
-from flask import Flask, render_template, url_for, Blueprint
+from flask import Flask, redirect, render_template, request, url_for, Blueprint
 from flask_cors import CORS
-
 bp = Blueprint('web', __name__,
                url_prefix='/web',
                static_folder='static/frontend',
@@ -11,36 +9,30 @@ bp = Blueprint('web', __name__,
 
 
 @bp.route('/')
-def web_index():
+def index():
     return render_template('index.html')
 
 
 @bp.route('/hello')  # for testing
-def web_hello():
+def hello():
     return 'Hello World!'
 
 
 # @login_required
 @bp.route('/dashboard', methods=['GET'])
-def web_dashboard():
+def dashboard():
     return render_template('dashboard.html')
 
 
 # @login_required
 @bp.route('/items', methods=['GET'])
-def web_items():
+def items():
     return render_template('items.html')
 
 
 # @login_required
-@bp.route('/record', methods=['GET'])
-def web_record():
-    return render_template('record.html')
-
-
-# @login_required
-@bp.route('/record', methods=['POST'])
-def web_record_post():
+@bp.route('/record', methods=['GET', 'POST'])
+def record():
     return render_template('record.html')
 
 
@@ -53,3 +45,53 @@ def web_login():
 @bp.route('/logout', methods=['GET'])
 def web_logout():
     return render_template('logout.html')
+
+
+@bp.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'GET':
+        return redirect(url_for('web.record'))
+
+    if request.files and 'file' in request.files:
+        file = request.files['file']
+        if file.filename == '':
+            return 'No selected file'
+
+    return 'File uploaded. File name: %s' % file.filename
+
+    body = request.form['body']
+    return body
+    # iteminfo = {
+    #     'title': body['title'],
+    #     'authors': body['authors'],
+    #     'year': body['year'],
+    #     'journal': body['journal'],
+    #     'volume': body['volume'],
+    #     'issue': body['issue'],
+    #     'pages': body['pages'],
+    #     'doi': body['doi'],
+    #     'abstract': body['abstract'],
+    #     'note': body['note'],
+    # }
+    # itemfile = body['file']  # bytes
+
+    # TODO: API upload process
+
+    if True:
+        return redirect(url_for('web.upload_succeed'))
+    else:
+        return redirect(url_for('web.web_error'))
+
+
+@bp.route('/upload_succeed', methods=['GET'])
+def upload_succeed():
+    return render_template('upload_succeed.html')
+
+
+@bp.route('/error', methods=['GET'])
+def error():
+    return render_template('error.html')
+
+@bp.route('/search', methods=['GET', 'POST'])
+def search():
+    return render_template('search.html')

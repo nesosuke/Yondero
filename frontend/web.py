091db1850f480bbb29b193857c4bf6d8c463b527
nesosuke <neso@neso.tech>
2022-04-28 23:25:01 +0900

    feat(frontend): :construction: web.py のパスを追加．

--- frontend/web.py
+++ frontend/web.py
@@ -35,3 +35,23 @@ data = {
     ]
 }
 # end of test data
+
+
+@bp.route('/')
+def index():
+    return render_template('index.html')
+
+
+@bp.route('/documents', methods=['GET', 'POST'])
+def documents_all():
+    return render_template('documents_all.html')
+
+
+@bp.route('documents/<document_id>', methods=['GET', 'PUT', 'DELETE'])
+def document(document_id):
+    return render_template('document.html', document_id=document_id)
+
+
+@bp.route('/documets/<document_id>/file', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def document_file(document_id):
+    return render_template('document_file.html', document_id=document_id)

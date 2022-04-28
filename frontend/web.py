4fa218960071e0b263d892a7cad386051c59fdad
nesosuke <neso@neso.tech>
2022-04-28 23:28:05 +0900

    refactor(frontend): :fire: いらんやつ削除

--- frontend/web.py
+++ frontend/web.py
@@ -1,40 +1,10 @@
-import requests
-from flask import Blueprint, Flask, redirect, render_template, request, url_for
-from flask_cors import CORS
+from flask import Blueprint, render_template
 
 bp = Blueprint('web', __name__,
                url_prefix='/web',
                static_folder='static/frontend',
                template_folder='templates/frontend',
                )
-api_url = 'http://localhost:8080/api/v1'
-
-# test data
-data = {
-    "doi": "10.2307/1969529",
-    "document_type": "article",
-    "title": "Non-Cooperative Games",
-    "authors": [
-        "John Nash"
-    ],
-    "journal": "Annals of Mathematics",
-    "volume": "54",
-    "issue": "2",
-    "pages": "286--295",
-    "year": "1951",
-    "abstract": "This is an abstract",
-    "keywords": [
-        "keyword1",
-        "keyword2"
-    ],
-    "url": "http://www.jstor.org/stable/1969529",
-    "note": "This is a note",
-    "tags": [
-        "tag1",
-        "tag2"
-    ]
-}
-# end of test data
 
 
 @bp.route('/')

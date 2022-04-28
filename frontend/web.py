81502d3fd5d70d725c4a457e18baf1700fe14108
nesosuke <neso@neso.tech>
2022-04-28 23:16:19 +0900

    test: test data の追加

--- frontend/web.py
+++ frontend/web.py
@@ -10,5 +10,28 @@ bp = Blueprint('web', __name__,
 api_url = 'http://localhost:8080/api/v1'
 
 # test data
-
+data = {
+    "doi": "10.2307/1969529",
+    "document_type": "article",
+    "title": "Non-Cooperative Games",
+    "authors": [
+        "John Nash"
+    ],
+    "journal": "Annals of Mathematics",
+    "volume": "54",
+    "issue": "2",
+    "pages": "286--295",
+    "year": "1951",
+    "abstract": "This is an abstract",
+    "keywords": [
+        "keyword1",
+        "keyword2"
+    ],
+    "url": "http://www.jstor.org/stable/1969529",
+    "note": "This is a note",
+    "tags": [
+        "tag1",
+        "tag2"
+    ]
+}
 # end of test data
